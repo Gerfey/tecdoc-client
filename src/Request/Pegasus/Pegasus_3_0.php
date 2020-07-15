e19334a66462509a3be0302a063bf5730eaeb723
Gerfey <sanjia@live.ru>
2020-07-15 17:58:35 +0700

    Update Pegasus_3_0.php

--- src/Request/Pegasus/Pegasus_3_0.php
+++ src/Request/Pegasus/Pegasus_3_0.php
@@ -7,34 +7,71 @@ use Gerfey\TecDoc\Request\Request;
 
 class Pegasus_3_0 extends Request
 {
-    public function getArticles(string $searchQuery, int $genericArticleId, int $searchType = 3, bool $oeNumbers = true, array $otherParams = []): ResponseTecDocInterface
-    {
+    public function getArticles(
+        string $searchQuery,
+        int $genericArticleId,
+        int $searchType = 3,
+        bool $oeNumbers = true,
+        array $otherParams = []
+    ): ResponseTecDocInterface {
         $this->function = 'getArticles';
-        return $this->createRequest('POST', [
-            'searchQuery' => $searchQuery,
-            'genericArticleId' => $genericArticleId,
-            'searchType' => $searchType,
-            'oeNumbers' => $oeNumbers,
-            $otherParams
-        ]);
+
+        return $this->createRequest(
+            'POST',
+            [
+                'searchQuery' => $searchQuery,
+                'genericArticleId' => $genericArticleId,
+                'searchType' => $searchType,
+                'oeNumbers' => $oeNumbers,
+                $otherParams,
+            ]
+        );
+    }
+
+    public function getDirectArticlesByIds6(array $articleIds)
+    {
+        $this->function = 'getDirectArticlesByIds6';
+
+        return $this->createRequest(
+            'POST',
+            [
+                'articleId' => ['array' => $articleIds],
+                'attributs' => true,
+                'basicData' => true,
+                'documents' => true,
+                'normalAustauschPrice' => false,
+                'oeNumbers' => true,
+                'replacedNumbers' => true,
+                'thumbnails' => true
+            ]
+        );
     }
 
     public function getAutoCompleteSuggestions(string $searchQuery): ResponseTecDocInterface
     {
         $this->function = 'getAutoCompleteSuggestions';
-        return $this->createRequest('POST', [
-            'searchQuery' => $searchQuery
-        ]);
+
+        return $this->createRequest(
+            'POST',
+            [
+                'searchQuery' => $searchQuery,
+            ]
+        );
     }
 
-    public function getArticleDirectSearchAllNumbersWithState(string $articleNumber, int $numberType = 0): ResponseTecDocInterface
-    {
+    public function getArticleDirectSearchAllNumbersWithState(
+        string $articleNumber,
+        int $numberType = 0
+    ): ResponseTecDocInterface {
         $this->function = 'getArticleDirectSearchAllNumbersWithState';
 
-        return $this->createRequest('POST', [
-            'articleNumber' => $articleNumber,
-            'numberType' => $numberType,
-            'searchExact' => true
-        ]);
+        return $this->createRequest(
+            'POST',
+            [
+                'articleNumber' => $articleNumber,
+                'numberType' => $numberType,
+                'searchExact' => true,
+            ]
+        );
     }
 }
