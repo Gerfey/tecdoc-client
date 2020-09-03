1df8f038096fe8c1445f4c6bc43a9d7363292257
Gerfey <sanjia@live.ru>
2020-09-03 08:48:44 +0700

    add to Response collection

--- src/Http/ResponseTecDoc.php
+++ src/Http/ResponseTecDoc.php
@@ -3,23 +3,37 @@
 namespace Gerfey\TecDoc\Http;
 
 use Gerfey\TecDoc\Contracts\Http\ResponseTecDocInterface;
+use Illuminate\Support\Collection;
 use Psr\Http\Message\ResponseInterface;
 
 class ResponseTecDoc implements ResponseTecDocInterface
 {
+    private $collect;
+
     private $response;
 
     public function __construct(ResponseInterface $response)
     {
+        $this->collect = new Collection();
+
         $this->response = $response;
     }
 
-    public function getJson()
+    public function getJson(): object
     {
         return json_decode($this->response->getBody()->getContents());
     }
 
-    public function getStatusCode()
+    public function getCollection(): Collection
+    {
+        return $this->collect->push($this->getJson()->data->array)->map(
+            function ($positions) {
+                return new Collection($positions);
+            }
+        )->first();
+    }
+
+    public function getStatusCode(): int
     {
         return $this->response->getStatusCode();
     }
